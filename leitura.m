2b79caf39554794737daaf7e9bd84c06a9f5a065
gabkyo <34278677+gabkyo@users.noreply.github.com>
2017-12-05 18:21:00 -0200

    Add files via upload

---
+++ leitura.m
@@ -0,0 +1,12 @@
+function [n, m, x1, xn, y1, ym, Nitermax, w, etol]=leitura
+  n=input("Digite n.")
+  m=input("Digite m.")
+  x1=input("Digite X1.")
+  xn=input("Digite Xn.")
+  y1=input("Digite Y1.")
+  ym=input("Digite Ym.")
+  Nitermax=input("Digite o numero maximo de iteracoes.")
+  w=input("Digite o parametro w de SOR.")
+  etol=input("Digite a tolerancia.")
+  endfunction
+  
